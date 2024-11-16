d0a3c1e513d7fafb5a942ef7fd3b4505605fa92e
Alexandra Pushkareva <pushkaryova@outlook.com>
2024-11-16 19:50:17 +0300

    updated product and skin_type pages

--- lib/screens/catalog_page/product_page.dart
+++ lib/screens/catalog_page/product_page.dart
@@ -2,32 +2,71 @@ import 'package:flutter/material.dart';
 
 import '../../models/product.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
   final String skinType;
-
-  final List<Product> products = [
-    Product('assets/images/product_page/product1.png', 'Сыворотка', 'Название',
-        '₽2300'),
-    Product('assets/images/product_page/product2.png', 'Тоник', 'Название',
-        '₽3400'),
-    Product(
-        'assets/images/product_page/product3.png', 'Крем', 'Название', '₽2000'),
-    Product(
-        'assets/images/product_page/product4.png', 'Гель', 'Название', '₽670'),
-    Product('assets/images/product_page/product5.png', 'Флюид', 'Название',
-        '₽4600'),
-    Product('assets/images/product_page/product6.png', 'Маска', 'Название',
-        '₽2600'),
-  ];
 
   ProductPage(this.skinType, {super.key});
 
   @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  int _selectedIndex = -1;
+
+  void _onCategoryTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  String getSkincarePhrase(String skinType) {
+    switch (skinType.toLowerCase()) {
+      case 'жирная':
+        return 'для жирной кожи';
+      case 'сухая':
+        return 'для сухой кожи';
+      case 'нормальная':
+        return 'для нормальной кожи';
+      case 'комбинированная':
+        return 'для комбинированной кожи';
+      case 'любой тип кожи':
+        return 'для любого типа кожи';
+      default:
+        return 'для кожи';
+    }
+  }
+
+  final List<Product> products = [
+    Product('assets/images/product_page/product1.png', 'Сыворотка', '₽2300',
+        'Сыворотка Christina'),
+    Product('assets/images/product_page/product2.png', 'Тоник', '₽3400',
+        'Тоник Christina'),
+    Product('assets/images/product_page/product3.png', 'Крем', '₽2000',
+        'Крем Christina'),
+    Product('assets/images/product_page/product4.png', 'Гель', '₽670',
+        'Гель Christina'),
+    Product('assets/images/product_page/product5.png', 'Флюид', '₽4600',
+        'Флюид Christina'),
+    Product('assets/images/product_page/product6.png', 'Маска', '₽2600',
+        'Маска Christina'),
+  ];
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Средства для $skinType кожи')),
+      appBar: AppBar(),
       body: Column(
         children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: Text('Средства\n${getSkincarePhrase(widget.skinType)}',
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold)),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Row(
@@ -36,13 +75,13 @@ class ProductPage extends StatelessWidget {
                 Text('${products.length} продуктов',
                     style: const TextStyle(fontSize: 18)),
                 const SizedBox(width: 50),
-                IconButton(
-                    icon: const Icon(Icons.filter_list), onPressed: () {}),
+                IconButton(icon: const Icon(Icons.tune), onPressed: () {}),
               ],
             ),
           ),
           _buildProductCategoryCarousel(
               ['Очищение', 'Увлажнение', 'Регенерация', 'Питание']),
+          const SizedBox(height: 5),
           Expanded(
             child: GridView.builder(
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -67,25 +106,38 @@ class ProductPage extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                      const SizedBox(height: 5),
                       ClipRRect(
                         borderRadius: const BorderRadius.vertical(
                             top: Radius.circular(10)),
                         child: Image.asset(product.imagePath,
-                            fit: BoxFit.cover,
-                            height: 120,
+                            fit: BoxFit.contain,
+                            height: 190,
                             width: double.infinity),
                       ),
                       Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(product.name,
-                            style: const TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold)),
+                        padding: const EdgeInsets.all(7.0),
+                        child: Text(
+                          product.category,
+                          style:
+                              const TextStyle(fontSize: 14, color: Colors.grey),
+                        ),
                       ),
                       Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Text(product.price,
-                            style: const TextStyle(
-                                fontSize: 14, color: Colors.grey)),
+                        padding: const EdgeInsets.symmetric(horizontal: 7.0),
+                        child: Text(
+                          product.name,
+                          style: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 7.0),
+                        child: Text(
+                          product.price,
+                          style:
+                              const TextStyle(fontSize: 14, color: Colors.grey),
+                        ),
                       ),
                     ],
                   ),
@@ -100,21 +152,26 @@ class ProductPage extends StatelessWidget {
 
   Widget _buildProductCategoryCarousel(List<String> categories) {
     return SizedBox(
-      height: 100,
+      height: 60,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: categories.length,
         itemBuilder: (context, index) {
-          return Container(
-            width: 100,
-            margin: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.blueAccent,
+          bool isSelected = index == _selectedIndex;
+          return GestureDetector(
+            onTap: () => _onCategoryTap(index),
+            child: Container(
+              width: 100,
+              margin: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: isSelected ? Colors.black : Colors.grey[200],
+              ),
+              child: Center(
+                  child: Text(categories[index],
+                      style: TextStyle(
+                          color: isSelected ? Colors.white : Colors.black))),
             ),
-            child: Center(
-                child: Text(categories[index],
-                    style: const TextStyle(color: Colors.white))),
           );
         },
       ),
--- lib/screens/catalog_page/skin_type_page.dart
+++ lib/screens/catalog_page/skin_type_page.dart
@@ -1,28 +1,39 @@
 import 'package:cosmetics_shop/screens/catalog_page/product_page.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/fade_animation.dart';
+
 class SkinTypePage extends StatelessWidget {
   const SkinTypePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Тип кожи')),
-      body: ListView(
-        children: [
-          _buildSkinTypeTile(context, 'Жирная'),
-          _buildSkinTypeTile(context, 'Комбинированная'),
-          _buildSkinTypeTile(context, 'Нормальная'),
-          _buildSkinTypeTile(context, 'Сухая'),
-          _buildSkinTypeTile(context, 'Любой тип'),
-        ],
+    return FadeAnimation(
+        delay: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('По типу кожи',
+              style: TextStyle(fontWeight: FontWeight.bold)),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: [
+            const SizedBox(height: 10),
+            _buildSkinTypeTile(context, 'Жирная'),
+            _buildSkinTypeTile(context, 'Комбинированная'),
+            _buildSkinTypeTile(context, 'Нормальная'),
+            _buildSkinTypeTile(context, 'Сухая'),
+            _buildSkinTypeTile(context, 'Любой тип'),
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildSkinTypeTile(BuildContext context, String skinType) {
     return ListTile(
-      title: Text(skinType),
+      title: Text(skinType,
+          style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 17)),
       onTap: () {
         Navigator.push(context,
             MaterialPageRoute(builder: (context) => ProductPage(skinType)));
